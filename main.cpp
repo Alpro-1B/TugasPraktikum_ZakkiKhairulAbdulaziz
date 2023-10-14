18d50274a708b144b9a4bda9bb0c9717fe6b2bc0
zelleienvil <2310631170122@student.unsika.ac.id>
2023-10-14 10:59:20 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a=70;
+    for(int c=1; c<=a; c++)
+    {
+        cout<<"-";
+    }
+    cout<<endl;
+    cout<<"Nama : Zakki Khairul Abdulaziz"<<endl<<endl;
+    cout<<"NPM : 2310631170122"<<endl<<endl;
+    cout<<"Kelas : B - Informatika"<<endl<<endl;
+    cout<<"Cita - Cita : Programmer"<<endl<<endl;
+    cout<<"Motivasi Masuk Informatika : Ingin belajar AI dan pemrograman aplikasi"<<endl<<endl;
+    int b=70;
+    for(int d=1; d<=b; d++)
+    {
+        cout<<"-";
+    }
+}
